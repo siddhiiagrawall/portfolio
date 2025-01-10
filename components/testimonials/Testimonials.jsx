a5bfc4cac16a930b1e5724b6fbf350a69bbd4c7c
Soumyajit Basak <116637467+soumyajiitt@users.noreply.github.com>
2025-01-10 12:48:28 +0530

    Testimonials Carousel Changes

--- components/testimonials/Testimonials.jsx
+++ components/testimonials/Testimonials.jsx
@@ -1,50 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './testimonial.css';
 import { Data } from './Data';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
 
 const Testimonials = () => {
-	return (
-		<section className='testimonial container section' id='testimonials'>
-			<h2 className='section__title'>My Colleagues Say</h2>
-			<span className='section__subtitle'>Testimonial</span>
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-			<Swiper
-				className='testimonial__container'
-				loop={true}
-				grabCursor={true}
-				spaceBetween={24}
-				pagination={{
-					clickable: true,
-				}}
-				breakpoints={{
-					576: {
-						slidesPerView: 2,
-					},
-					768: {
-						slidesPerView: 2,
-						spaceBetween: 48,
-					},
-				}}
-				modules={[Pagination]}
-			>
-				{Data.map(({ id, image, title, description }) => {
-					return (
-						<SwiperSlide className='testimonial__card' key={id}>
-							<img src={image} alt='' className='testimonial__img' />
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % Data.length);
+    }, 5000); // Change testimonial every 5 seconds
 
-							<h3 className='testimonial__name'>{title}</h3>
-							<p className='testimonial__description'>{description}</p>
-						</SwiperSlide>
-					);
-				})}
-			</Swiper>
-		</section>
-	);
+    return () => clearInterval(interval);
+  }, []);
+
+  const nextTestimonial = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % Data.length);
+  };
+
+  const prevTestimonial = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + Data.length) % Data.length);
+  };
+
+  return (
+    <section className='testimonial container section' id='testimonials'>
+      <h2 className='section__title'>My Colleagues Say</h2>
+      <span className='section__subtitle'>Testimonial</span>
+
+      <div className='testimonial__carousel'>
+        <button className='carousel__button carousel__button--prev' onClick={prevTestimonial}>
+          &lt;
+        </button>
+        <div className='testimonial__card'>
+          <img src={Data[currentIndex].image} alt='' className='testimonial__img' />
+          <h3 className='testimonial__name'>{Data[currentIndex].title}</h3>
+          <p className='testimonial__description'>{Data[currentIndex].description}</p>
+        </div>
+        <button className='carousel__button carousel__button--next' onClick={nextTestimonial}>
+          &gt;
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
