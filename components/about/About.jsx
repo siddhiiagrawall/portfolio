e56ebcc8a7dd003021254908809d8ca8fa6398e9
Soumyajit Basak <116637467+soumyajiitt@users.noreply.github.com>
2025-03-18 12:08:25 +0530

    Resume Update

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import "./about.css";
 import AboutImg from "../../src/assets/about-pic.png";
-import CV from "../../src/assets/Soumyajit's New Resume.pdf";
+import CV from "../../src/assets/Soumyajit's Resume.pdf"
 import Info from './Info';
 
 const About = () => {
